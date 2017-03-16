6980d0f439a85863b79a380005863013f86662de
jerryzejia <jerryzejia@gmail.com>
2017-03-16 18:27:55 -0400

    refactor

--- Header/macros.h
+++ Header/macros.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   macros.h
  * Author: Administrator
  *
@@ -8,12 +8,23 @@
 #ifndef MACROS_H
 #define	MACROS_H
 
-#define __delay_1s() for(char i=0;i<10;i++){__delay_ms(98);}
+#define __delay_1s()                                                           \
+  for (char i = 0; i < 100; i++) {                                             \
+    __delay_ms(10);                                                            \
+  }
 #define __lcd_newline() lcdInst(0b11000000);
-#define __lcd_clear() lcdInst(0b10000000);
+#define __lcd_clear() lcdInst(0x01);
 #define __lcd_home() lcdInst(0b10000000);
-#define __bcd_to_num(num) (num & 0x0F) + ((num & 0xF0)>>4)*10
+#define LCD_LINE_1_ADDRESS 0x00
+#define LCD_LINE_2_ADDRESS 0x20
+#define LCD_LINE_3_ADDRESS lcdInst(11000000)
+#define LCD_LINE_4_ADDRESS 0x60
+
+#define TIN_LAB 1
+#define TIN_NOLAB 2
+#define POP_TAB 3
+#define POP_NOTAB 4
+
 
 
 #endif	/* MACROS_H */
-
--- main.c
+++ main.c
@@ -8,26 +8,10 @@
 #include "configBits.h"
 #include "constants.h"
 #include "lcd.h"
+#include "macros.h"
 #include "pwm.h"
 #include <stdio.h>
 #include <xc.h>
-
-#define __delay_1s()                                                           \
-  for (char i = 0; i < 100; i++) {                                             \
-    __delay_ms(10);                                                            \
-  }
-#define __lcd_newline() lcdInst(0b11000000);
-#define __lcd_clear() lcdInst(0x01);
-#define __lcd_home() lcdInst(0b10000000);
-#define LCD_LINE_1_ADDRESS 0x00
-#define LCD_LINE_2_ADDRESS 0x20
-#define LCD_LINE_3_ADDRESS lcdInst(11000000)
-#define LCD_LINE_4_ADDRESS 0x60
-
-#define TIN_LAB 1
-#define TIN_NOLAB 2
-#define POP_TAB 3
-#define POP_NOTAB 4
 
 void set_time(void);
 void get_time(unsigned char *);
